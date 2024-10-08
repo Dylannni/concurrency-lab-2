62bdb7f8e73ff7daf714b419174756c4aa4f0c60
Dylan Lin <dylanlin0411@gmail.com>
2024-10-08 21:08:38 +0100

    Completed upto Question2

--- pc/pc.go
+++ pc/pc.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	//"github.com/ChrisGora/semaphore"
 	"math/rand"
+	"sync"
 	"time"
+
+	"github.com/ChrisGora/semaphore"
 )
 
 type buffer struct {
@@ -34,30 +36,51 @@ func (buffer *buffer) put(x int) {
 	buffer.write = (buffer.write + 1) % len(buffer.b)
 }
 
-func producer(buffer *buffer, start, delta int) {
+func producer(buffer *buffer, spaceAvailable, workAvailable semaphore.Semaphore, mutex *sync.Mutex, start, delta int) {
 	x := start
 	for {
+		spaceAvailable.Wait() // Wait until there is space available in the buffer
+		mutex.Lock()          // Lock the buffer to ensure exclusive access
 		buffer.put(x)
+		workAvailable.Post() // Signal that work is avaible
+		mutex.Unlock()       // Unlock the buffer to ensure other can access
+
 		x = x + delta
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 	}
 }
 
-func consumer(buffer *buffer) {
+func consumer(buffer *buffer, spaceAvailable, workAvailable semaphore.Semaphore, mutex *sync.Mutex) {
 	for {
+		workAvailable.Wait()
+		mutex.Lock()
 		_ = buffer.get()
+		spaceAvailable.Post()
+		mutex.Unlock()
+
 		time.Sleep(time.Duration(rand.Intn(5000)) * time.Millisecond)
 	}
 }
 
 func main() {
+	// 2a
+	// 4, 994, 5
+
+	// 2c How might you improve the buffer methods
+	// 1. Use RWMutex, so that there's mulitple read rountine, improve concurrency
+	// 2. Condition Variables
+
 	buffer := newBuffer(5)
+	mutex := &sync.Mutex{} // Initialse a mutex
+	// var mutex sync.Mutex
 
-	//spaceAvailable := semaphore.Init(5, 5)
-	//workAvailable := semaphore.Init(5, 0)
+	spaceAvailable := semaphore.Init(5, 5) // Initialise a semaphore for spaces available
+	workAvailable := semaphore.Init(5, 0)  // Initialise a semaphore for work available
 
-	go producer(&buffer, 1, 1)
-	go producer(&buffer, 1000, -1)
+	// Start two producer goroutines with different starting values and increments.
+	go producer(&buffer, spaceAvailable, workAvailable, mutex, 1, 1)
+	go producer(&buffer, spaceAvailable, workAvailable, mutex, 1000, -1)
 
-	consumer(&buffer)
+	consumer(&buffer, spaceAvailable, workAvailable, mutex)
+	// ${[contents] length start_index end_index}
 }
--- sum/sum.go
+++ sum/sum.go
@@ -8,14 +8,30 @@ import (
 func main() {
 	sum := 0
 	var wg sync.WaitGroup
+	chans := make(chan int)
+
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
+			chans <- 0
 			sum = sum + 1
 			wg.Done()
 		}()
+		<-chans
 	}
 
+	// for i := 0; i < 1000; i++ {
+	// 	wg.Add(1)
+	// 	go func() {
+	// 		chans <- 0
+	// 		sum = sum + 1
+	// 		wg.Done()
+	// 	}()
+	// }
+
+	// for i := 0; i < 1000; i++ {
+	// 	<-chans
+	// }
 	wg.Wait()
 	fmt.Println(sum)
 }
